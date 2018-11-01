ce819e655069541ff5bedbf195844e770d6a3742
Rune Ibsen <rui@copenhagensoftware.com>
2018-11-01 08:32:22 +0100

    Refactor directoriesToRoot

--- AdrCli/FileSystem.fs
+++ AdrCli/FileSystem.fs
@@ -6,11 +6,24 @@ module FileSystem =
     let private isProjectRootDirectory (dir : DirectoryInfo) =
         dir.GetDirectories() |> Seq.exists (fun d -> d.Name = ".adr")
 
-    let rec directoriesToRoot =
-        Seq.unfold (fun (dir : DirectoryInfo) -> match dir with | null -> None | _ -> Some (dir, dir.Parent))
+    let rec private follow next x =
+        seq {
+            yield x
+            yield! follow next (next x) 
+        }
+
+    let private parent (d : DirectoryInfo) = d.Parent
+
+    let directoriesToRoot dir =
+        let rec follow next x = seq {
+             yield x; yield! follow next (next x)
+        }
+        dir |> follow parent |> Seq.takeWhile (not << isNull)
 
     let getSettingsDirectory (currentDir : DirectoryInfo) : DirectoryInfo option =
         currentDir |> directoriesToRoot |> Seq.tryFind isProjectRootDirectory 
 
-    let init (currentDir : DirectoryInfo) =
-        currentDir.CreateSubdirectory(".adr")
+    let init (currentDir : DirectoryInfo) (repositoryPath : string option) =
+        currentDir.CreateSubdirectory(".adr") |> ignore
+        if(repositoryPath |> Option.isSome) then
+            System.IO.Directory.CreateDirectory(repositoryPath |> Option.get) |> ignore
