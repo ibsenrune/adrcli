5724ea9200f5d633aafe0da269f4249b608c553e
Rune Ibsen <rui@copenhagensoftware.com>
2018-11-01 18:43:39 +0100

    Add add command

--- AdrCli/Cli.fs
+++ AdrCli/Cli.fs
@@ -18,6 +18,12 @@ module Cli =
         let optionPath = cmd.Option("-p|--path <PATH>", "The path to the directory where you want to store your ADR files.", CommandOptionType.SingleValue)
         cmd.OnExecute(fun () -> FileSystem.init FileSystem.cwd (ofOption optionPath)) |> ignore
 
+    let private addAction (cmd : CommandLineApplication) =
+        cmd.HelpOption() |> ignore
+        let nameOption = cmd.Option("-n|--name", "The title of the ADR document you want to add", CommandOptionType.SingleValue)
+        cmd.OnExecute(fun () -> ()) |> ignore
+
     let app = CommandLineApplication()
     app.HelpOption() |> ignore
     app.Command("init", initAction) |> ignore
+    app.Command("add", addAction) |> ignore
