fa761641893ac4c7eb874a2b82891bc956006297
Rune Ibsen <rui@copenhagensoftware.com>
2018-11-03 14:43:20 +0100

    Add methods to serialise and deserialise settings

--- AdrCli/FileSystem.fs
+++ AdrCli/FileSystem.fs
@@ -1,8 +1,12 @@
 namespace AdrCli
 
 open System.IO
+open System.Text.RegularExpressions
 
 module FileSystem =
+
+    type Settings = { repositoryPath : string }
+
     let private isProjectRootDirectory (dir : DirectoryInfo) =
         dir.GetDirectories() |> Seq.exists (fun d -> d.Name = ".adr")
 
@@ -19,7 +23,15 @@ module FileSystem =
 
     let cwd = System.Environment.CurrentDirectory |> DirectoryInfo
 
-    let init (currentDir : DirectoryInfo) (repositoryPath : string option) =
+    let init (currentDir : DirectoryInfo) (settings : Settings) =
         currentDir.CreateSubdirectory(".adr") |> ignore
-        if(repositoryPath |> Option.isSome) then
-            System.IO.Directory.CreateDirectory(repositoryPath |> Option.get) |> ignore
+        System.IO.Directory.CreateDirectory(settings.repositoryPath) |> ignore
+
+    let serialiseSettings (s : Settings) =
+       sprintf "settings:\n    repositoryPath: %s\n"  s.repositoryPath
+
+    let deserialiseSettings (s : string) =
+        let m = System.Text.RegularExpressions.Regex.Match(s, "^    repositoryPath: (.+)$", RegexOptions.Multiline)
+        if (m.Success) 
+        then Some ({ repositoryPath = m.Groups.[1].Value })
+        else None
